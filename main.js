815291ff63096ee65362bbbddbb52c3f5e42ca9d
emmanuelportes <mportesz.08@gmail.com>
2021-01-19 13:11:57 -0400

    Second commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const mainTable = document.getElementById("main-table");
+const addNewRow = document.getElementById("add-row");
+const addNewColumn = document.getElementById("add-column");
+const deleteButton = document.getElementById("delete");
+
+// function generateFirstTableHeader(){}
+// function generateTableRows(){ }
+// function generateTableColumns(){ }
+
+//function countTableRows(){ }
+
+//function countTableCells(){ }
+
