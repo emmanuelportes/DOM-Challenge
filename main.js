2714d038fbeb2e28f44e98eb6373bf56acc73435
emmanuelportes <mportesz.08@gmail.com>
2021-01-19 22:08:36 -0400

    Header creation functionality created

--- main.js
+++ main.js
@@ -3,8 +3,23 @@ const addNewRow = document.getElementById("add-row");
 const addNewColumn = document.getElementById("add-column");
 const deleteButton = document.getElementById("delete");
 
-// function generateFirstTableHeader(){}
+function generateFirstTableHeader(){
+
+    const tableRow = document.createElement("tr");
+    const tableHeader1 = document.createElement("th");
+    const tableHeader2 = document.createElement("th");
+    const headerText = document.createTextNode("Last Column");
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+
+    tableHeader1.appendChild(checkbox);
+    tableHeader2.appendChild(headerText);
+    tableRow.append(tableHeader1, tableHeader2);
+    mainTable.appendChild(tableRow);
+
+}
 // function generateTableRows(){ }
+
 // function generateTableColumns(){ }
 
 //function countTableRows(){ }
