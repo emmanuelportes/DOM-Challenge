53cf1230dbd001d42a2494858efdcc2fce7218e4
emmanuelportes <mportesz.08@gmail.com>
2021-03-06 23:45:42 -0400

    make table cells editable

--- main.js
+++ main.js
@@ -1,103 +1,109 @@
-const mainTable = document.getElementById("main-table");
-const addNewRow = document.getElementById("add-row");
-const addNewColumn = document.getElementById("add-column");
+const table = document.getElementById("main-table");
+const addRowButton = document.getElementById("add-row");
+const addColumnButton = document.getElementById("add-column");
 const deleteButton = document.getElementById("delete");
 
+function generateTableHeader(){
 
-
-function generateFirstTableHeader(){
-
-    const tableRow = document.createElement("tr");
-    const tableHeader1 = document.createElement("th");
-    const tableHeader2 = document.createElement("th");
-    const headerText = document.createTextNode("Last Column");
+    const row = document.createElement("tr");
+    const firstHeader = document.createElement("th");
+    const secondHeader = document.createElement("th");
     const checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    tableHeader1.appendChild(checkbox);
-    tableHeader2.appendChild(headerText);
-    tableRow.append(tableHeader1, tableHeader2);
-    mainTable.appendChild(tableRow);
-
+    checkbox.setAttribute("type","checkbox");
+    firstHeader.appendChild(checkbox);
+    secondHeader.innerText = "Last Column";
+    row.append(firstHeader, secondHeader);
+    table.appendChild(row);
+ 
 }
 
 function generateTableRows(){
-    tableRow = document.createElement("tr");
 
-    for(let index = 1; index <= countTableCells() ; index++){
+    let rows = document.createElement("tr");
 
-        
-        firstTableCell = document.createElement("td");
-        middleTableCell = document.createElement("td");
-        lastTableCell = document.createElement("td");
-        functionsButton = document.createElement("button");
-        functionsButton.innerHTML = "BTN";
-        checkbox = document.createElement("input");
+    for(let index = 1; index <= getCellsLength() ; index++){
+
+        const firstCells = document.createElement("td");
+        const middleCells = document.createElement("td");
+        const lastCells = document.createElement("td");
+        const button = document.createElement("button");
+        const checkbox = document.createElement("input");
         checkbox.setAttribute("type", "checkbox");
-        firstTableCell.appendChild(checkbox);
-        lastTableCell.appendChild(functionsButton);
+        makeCellsEditable(middleCells);
+        button.innerText = "Button";
+        firstCells.appendChild(checkbox);
+        lastCells.appendChild(button);
 
         if(index === 1){
+            rows.appendChild(firstCells);
 
-            tableRow.appendChild(firstTableCell);
-
-        } else if (index === countTableCells()){
-
-            tableRow.appendChild(lastTableCell);
+        } else if (index === getCellsLength()){
+            rows.appendChild(lastCells);
 
         } else {
-
-            tableRow.appendChild(middleTableCell);
+            rows.appendChild(middleCells);
 
         }
-
-
-
     }
 
-    mainTable.appendChild(tableRow);
+    table.appendChild(rows)
  }
 
  function AddNewRow(){
-    if(mainTable.childNodes.length < 1){
-        generateFirstTableHeader();
+
+    if(table.children.length < 1){
+       generateTableHeader();
     }else{
         generateTableRows();
     }
 }
 
 function generateTableColumns(){
-
     let rows = document.querySelectorAll("tr");
-    for(let i = 0; i < countTableRows(); i++){
+ 
+    for(let i = 0; i < getRowsLength(); i++){
         if(i === 0){
 
-            tableHeader = document.createElement("th");
-            tableHeaderText = document.createTextNode("new column header");
-            tableHeader.appendChild(tableHeaderText);
-            rows[i].insertBefore(tableHeader, rows[i].lastChild);
+            header = document.createElement("th");
+            headerText = document.createTextNode("new column header");
+            header.appendChild(headerText);
+            rows[i].insertBefore(header, rows[i].lastChild);
 
         } else {
-              
-        tableCell = document.createElement("td");
-        tableCellText = document.createTextNode("New Column");
-        tableCell.appendChild(tableCellText);
-        rows[i].insertBefore(tableCell, rows[i].lastChild);  
 
-        }
-    
-   }
+        cells = document.createElement("td");
+        cellsText = document.createTextNode("New Column");
+        makeCellsEditable(cells);
+        cells.appendChild(cellsText);
+        rows[i].insertBefore(cells, rows[i].lastChild);
 
- }
+        } 
+    }
+
+}
+
+function makeCellsEditable(cell){
+    cell.setAttribute("contenteditable","false");
+
+    cell.addEventListener("dblclick", () => {
+        cell.contentEditable = true;
+    });
+
+    cell.addEventListener("keydown", e =>{
+        
+    });
+ 
+}
 
 
-function countTableRows(){ 
-   return mainTable.getElementsByTagName("tr").length;
+
+function getRowsLength(){ 
+   return table.getElementsByTagName("tr").length;
    //console.log(mainTable.getElementsByTagName("tr").length);
    
 };
 
-function countTableCells(){ 
-   return mainTable.getElementsByTagName("th").length;
+function getCellsLength(){ 
+   return table.getElementsByTagName("th").length;
     //console.log(mainTable.getElementsByTagName("th").length);
 }
-
